e9b7f4ebaaa8e9f14078e95717478021f164191b
Yazid <yazyyazid@gmail.com>
2024-02-06 10:19:15 +0800

    change output path

--- scraper.py
+++ scraper.py
@@ -132,7 +132,7 @@ def get_shopurl(url):
 
     df = pd.DataFrame.from_dict(data, orient="index").transpose()
     df.to_csv(
-        f'/Users/yazid/dev/webscraping_etl/script/output csv/petakopi_data_{now.strftime("%d-%m-%Y")}.csv',
+        f'petakopi_data_{now.strftime("%d-%m-%Y")}.csv',
         index=False,
     )
     print(f'CSV EXPORTED! - {now.strftime("%H:%M:%S")}')
